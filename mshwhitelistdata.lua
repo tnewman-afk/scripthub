07cecb92a948f975472089f67956a33f083860a3
Javalsta <91215378+tnewman-afk@users.noreply.github.com>
2024-04-09 20:11:25 +0100

    Update mshwhitelistdata.lua

--- mshwhitelistdata.lua
+++ mshwhitelistdata.lua
@@ -1,10 +1,9 @@
-local whitelist = {
-{ID = 4124339158, Username = "TheGrantedLegend", HWID = "62f7ad54-31ad-4c31-b012-021add1d77dd"},
-{ID = 4124339158, Username = "TheGrantedLegend", HWID = "54D927F2-2E9D-4F9D-AB39-E8A02496D8B7"},
-{ID = 69285840, Username = "lancerss00", HWID = "e8efda87-5774-4ffb-bc1a-2153f5e82a7d"},
-{ID = 123456789, Username = "123456789", HWID = "123456789"},
-{ID = 123456789, Username = "123456789", HWID = "123456789"},
-{ID = 123456789, Username = "123456789", HWID = "123456789"},
-
--- Add more entries here as needed
+return {
+    {ID = 4124339158, Username = "TheGrantedLegend", HWID = "62f7ad54-31ad-4c31-b012-021add1d77dd"},
+    {ID = 4124339158, Username = "TheGrantedLegend", HWID = "54D927F2-2E9D-4F9D-AB39-E8A02496D8B7"},
+    {ID = 69285840, Username = "lancerss00", HWID = "e8efda87-5774-4ffb-bc1a-2153f5e82a7d"},
+    {ID = 123456789, Username = "123456789", HWID = "123456789"},
+    {ID = 123456789, Username = "123456789", HWID = "123456789"},
+    {ID = 123456789, Username = "123456789", HWID = "123456789"},
+    -- Add more entries here as needed
 }
